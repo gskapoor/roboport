38ce9aa55b81fbe409c85b6034e76f53c674a675
zoistie <68969173+zoistie@users.noreply.github.com>
2022-07-30 00:38:51 -0700

    Update playerMovement.cs

--- src/Assets/Scripts/playerMovement.cs
+++ src/Assets/Scripts/playerMovement.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     [SerializeField]private float speed;
+    [SerializeField]private float jumpSpeed;
     private Rigidbody2D body;
+    
 
     private void Awake()
     {
@@ -11,10 +13,17 @@ public class playerMovement : MonoBehaviour
     }
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal")*speed,body.velocity.y);
+        //Player L&R movement
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput*speed,body.velocity.y);
+        //Flip Player
+        if (horizontalInput > 0)
+            transform.localScale = new Vector3(transform.localScale.x,transform.localScale.y,transform.localScale.z);
+        else if (horizontalInput < 0)
+            transform.localScale = new Vector3(-1*transform.localScale.x,transform.localScale.y,transform.localScale.z);
+        //PLayer Jump
         if(Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x,speed);
+            body.velocity = new Vector2(body.velocity.x,jumpSpeed);
     }
 
 }
-
