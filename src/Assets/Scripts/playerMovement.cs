87ab42932a4852b5dd321c3f225a4cfdd81f8f7e
Gobind Kapoor <kapoorgobind@gmail.com>
2022-07-30 01:15:58 -0700

    fixed rot error + serialized fields

--- src/Assets/Scripts/playerMovement.cs
+++ src/Assets/Scripts/playerMovement.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    [SerializeField]private float speed;
-    [SerializeField]private float jumpSpeed;
+    [SerializeField]private float speed = 1;
+    [SerializeField]private float jumpSpeed = 1;
+    [SerializeField]private bool isRight = true;
     private Rigidbody2D body;
     
 
@@ -17,10 +18,14 @@ public class playerMovement : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         body.velocity = new Vector2(horizontalInput*speed,body.velocity.y);
         //Flip Player
-        if (horizontalInput > 0)
+        if (!isRight && horizontalInput > 0){
             transform.localScale = new Vector3(transform.localScale.x,transform.localScale.y,transform.localScale.z);
-        else if (horizontalInput < 0)
+            isRight = true;
+        }
+        else if (isRight && horizontalInput < 0){
             transform.localScale = new Vector3(-1*transform.localScale.x,transform.localScale.y,transform.localScale.z);
+            isRight = false;
+        }
         //PLayer Jump
         if(Input.GetKey(KeyCode.Space))
             body.velocity = new Vector2(body.velocity.x,jumpSpeed);
