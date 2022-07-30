4ee6aa8beca34b27cf68db9aa5c49f9e3cb58cab
zoistie <68969173+zoistie@users.noreply.github.com>
2022-07-29 23:20:47 -0700

    testScene

--- src/Assets/Scripts/playerMovement.cs
+++ src/Assets/Scripts/playerMovement.cs
@@ -1,18 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D body;
+
+    private void Awake()
     {
-        
+        body = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal"),body.velocity.y);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
+
