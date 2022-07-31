3bf1a06f046f936e5d921656ebbb5462c5195cbf
Gobind Kapoor <kapoorgobind@gmail.com>
2022-07-31 11:52:45 -0700

    edited remove terrain script

--- src/Assets/Scripts/removeTerrain.cs
+++ src/Assets/Scripts/removeTerrain.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class removeTerrain : MonoBehaviour
 {
+    Tilemap tilemap;
     // Start is called before the first frame update
     void Start()
     {
-        
+        tilemap = GetComponent<Tilemap>();
     }
 
     // Update is called once per frame
@@ -16,10 +18,21 @@ public class removeTerrain : MonoBehaviour
         
     }
 
-    void OnCollisionEnter2D (Collision2D collision)
+    void OnTriggerEnter2D (Collider2D collision)
     {
-        if (collision.gameObject.ToString() == "bullet"){
-            Destroy(gameObject);
+        ContactPoint2D[] contacts = new ContactPoint2D[1];
+        if (collision.gameObject.name == "bullet(Clone)"){
+            Vector3 hitPosition = Vector3.zero;
+            collision.GetContacts(contacts);
+            hitPosition.x = contacts[0].point.x;
+            hitPosition.y = contacts[0].point.y;
+            Debug.Log("bruh");
+            tilemap.SetTile(tilemap.WorldToCell(hitPosition), null);
+
+
+        }
+        else{
+            Debug.Log(collision.gameObject.name);
         }
     }
 
